170941c8305d9d9dffad56ba33cc242cfc422531
ajc6432 <ajc6432@protonmail.com>
2026-02-22 20:36:10 -0800

    - Remove Cocoapods and Carthage refs - Re-structure foles and folders - Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,28 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
     name: "Pitchy",
-    platforms: [
-        .iOS(.v18),
-        .macOS(.v14)
+    platforms: [.iOS(.v18), .macOS(.v15)],
+    products: [
+        .library(name: "Pitchy", targets: ["Pitchy"])
     ],
-    products: [ .library(name: "Pitchy", targets: ["Pitchy"]) ],
-    targets: [ .target(name: "Pitchy", path: "Source") ]
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", from: "7.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "13.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Pitchy",
+            dependencies: [],
+            path: "Sources/Pitchy",
+            swiftSettings: [.swiftLanguageMode(.v6)]
+        ),
+        .testTarget(
+            name: "PitchyTests",
+            dependencies: ["Pitchy", "Quick", "Nimble"],
+            path: "Tests/PitchyTests",
+            swiftSettings: [.swiftLanguageMode(.v5)] // update to 6 later
+        )
+    ]
 )
